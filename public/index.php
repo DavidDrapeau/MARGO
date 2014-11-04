dc204722c8eafde4b199419498fdba904f5cddd2
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2014-11-04 11:22:15 +0100

    Index redirection

--- public/index.php
+++ public/index.php
@@ -1,29 +1,24 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
-	<head>
-		<title> MARGO | Authentification </title>
-		<link rel="stylesheet" href="../Webroot/css/style.css" />
-		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/bootstrap-theme.css" />
+<?php
 
-	</head>
+require_once("../conf/fonctions.inc.php");
 
-	<body>
-		<div class="auth">
-		<h2> MARGO | ESPACE ADMIN </h2>
-		<hr>
-		<form method="post" action="#">
-		<label> Login  </label><span class="push-log"></span><input type="text"></input><br />
-		<p></p>	
-		<label>Mot de passe  </label><span class="push-pswd"></span><input type="password"></input><br /><br />
-		<input class="button" type="submit" value="Se connecter" />
-		</form>
-		</div>
-	</body>
-</html>
+// Réception des paramètres
+if (isset($_GET['controleur'])) {
+    $controleur = $_GET['controleur'];
+} else {
+    $controleur = 'accueil';
+}
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'defaut';
+}
+
+// Construction du nom de la classe contrôleur
+$classeControleur = 'C_' . ucfirst($controleur);
+// Instanciation
+$ctrl = new $classeControleur();
+// Appel de la méthode d'action
+$ctrl->$action();
+
+?>
