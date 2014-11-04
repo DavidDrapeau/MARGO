48de23e82226b17020fcebda82c4636fcc6f7b36
btssio <btssio@btssio-VirtualBox>
2014-11-04 11:43:28 +0100

    accueil

--- Controleur/C_Accueil.php
+++ Controleur/C_Accueil.php
@@ -4,12 +4,12 @@ class C_accueil{
     
     function accueil() {
         
-        $uneVue = new V_Vue(RACINE."/vue/template.inc.php" );
+        $uneVue = new V_Vue("../Vue/template.inc.php" );
         $uneVue->ajouter('titre', 'Authentification');
-        $uneVue->ajouter('entete',RACINE."/vue/vueEntete.inc.php");
-        $uneVue->ajouter('gauche',RACINE."/vue/vueGauche.inc.php");
-        $uneVue->ajouter('centre',RACINE."/vue/vueCentreAccueil.inc.php");
-        $uneVue->ajouter('pied', RACINE."/vue/vuePied.inc.php");
+        $uneVue->ajouter('entete',"../Vue/vueEntete.inc.php");
+        $uneVue->ajouter('gauche',"/Vue/vueGauche.inc.php");
+        $uneVue->ajouter('centre',"/Vue/vueCentreAccueil.inc.php");
+        $uneVue->ajouter('pied', "/Vue/vuePied.inc.php");
             
         $uneVue->afficher();
     }
