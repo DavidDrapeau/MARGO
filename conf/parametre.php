6169518e589eb47ffef63e8883bd6c1a8b9e95e0
btssio <btssio@btssio-VirtualBox>
2014-11-04 11:18:04 +0100

    ajout d'autoload et fichier configuration bdd

---
+++ conf/parametre.php
@@ -0,0 +1,11 @@
+<?php
+
+// Version
+define('VERSION_APPLICATION', 'juin 2014 - initiale');
+
+// Accès base de données
+define('DSN', 'mysql:host=localhost;dbname=MARGO');
+define('USER', 'root');
+define('MDP', 'joliverie');
+
+
