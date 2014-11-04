acbfae3477017b77e5c281c22c66f758e97b9d6a
btssio <btssio@btssio-VirtualBox>
2014-11-04 12:46:23 +0100

    Etudiant - 2eme Etape

--- Modele/Dao/Dao_Etudiant.php
+++ Modele/Dao/Dao_Etudiant.php
@@ -1,4 +1,19 @@
 <?php
 
-
+class M_DaoEtudiant implements M_DaoGenerique{
+    public static function enregistrementVersObjet($unEnregistrement) {
+        
+    }
+    public static function objetVersEnregistrement($objetMetier) {
+        
+    }
+    
+    public static function getAll($pdo) {
+        
+    }
+    
+    public static function getOneById($valeurClePrimaire){
+        
+    }
+}
 
