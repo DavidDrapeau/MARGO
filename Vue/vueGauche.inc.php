d201dd4a9c17bff2dede2f4ca2eaeca7045ecfc8
btssio <btssio@btssio-VirtualBox>
2014-11-18 11:40:38 +0100

    Merge origin/master
    Conflicts:
    	Vue/vueGauche.inc.php

--- Vue/vueGauche.inc.php
+++ Vue/vueGauche.inc.php
@@ -25,35 +25,3 @@
 				</ul>
 			</div>
 		</nav>
-
-<nav>
-			<div class="nav-bar">
-				<h3> Menu Principal</h3>
-				<ul>
-					<li><span class="glyphicon glyphicon-home"> </span> <a href="#">Accueil</a></li>
-                                        <h3>Enseignement</h3>
-                                        <hr>
-					<li><span class="glyphicon glyphicon-bookmark"> </span> <a href="?controleur=Enseignement&action=afficher">Liste enseignements</a></li>                                     
-                                        <li><span class="glyphicon glyphicon-bookmark"> </span> <a href="?controleur=Enseignement&action=ajouter">Ajouter un enseignements</a></li>
-                                        
-                                        <h3>Filières</h3>
-                                        <hr>
-					<li><span class="glyphicon glyphicon-tasks"> </span> <a href="#">Liste Filières</a></li>
-                                        <li><span class="glyphicon glyphicon-tasks"> </span> <a href="#">Ajouter Filières</a></li>
-                                        
-                                        <h3>Classes</h3>
-                                        <hr>
-					<li><span class="glyphicon glyphicon-briefcase"> </span> <a href="?controleur=Classe&action=afficher">Liste classes</a></li>
-                                        <li><span class="glyphicon glyphicon-briefcase"> </span> <a href="?controleur=Classe&action=ajouter">Ajouter classes</a></li>
-                                        
-					<li><span class="glyphicon glyphicon-briefcase"> </span> <a href="?controleur=Enseignement&action=show">Liste classes</a></li>
-                                        <li><span class="glyphicon glyphicon-briefcase"> </span> <a href="?controleur=Enseignement&action=ajouter">Ajouter classes</a></li>
-                                     
-                                        <h3>Elèves</h3>
-                                        <hr>
-                                        <li><span class="glyphicon glyphicon-user"> </span> <a href="#">Liste lèves</a></li>
-					<li><span class="glyphicon glyphicon-user"> </span> <a href="#">Ajouter Elèves</a></li>
-				</ul>
-			</div>
-		</nav>
-
