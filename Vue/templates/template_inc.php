67c555e1b70807534133c6be12291c1d34e7c083
btssio <btssio@btssio-VirtualBox>
2014-12-08 14:36:17 +0100

    modif lien vers css et js

--- Vue/templates/template_inc.php
+++ Vue/templates/template_inc.php
@@ -8,8 +8,8 @@ and open the template in the editor.
         <meta charset="UTF-8">
        
 		<meta charset='UTF-8' />
-		<link rel="stylesheet" href="../Webroot/css/style.css" />
-		<link rel="stylesheet" href="../Webroot/css/bootstrap/bootstrap-theme.css" />
+		<link rel="stylesheet" href="../Webroot/css/bootstrap.css" />
+		<link rel="stylesheet" href="../Webroot/css/bootstrap-theme.css" />
 		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 		<title>Margo | Espace enseignant</title>
 	
@@ -30,6 +30,10 @@ and open the template in the editor.
                 <?php include($this->lire('pied'));?>
             </div>
         </div>
+              <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript" src="../Webroot/js/bootstrap.js"></script>
+  
+       
     </body>
 </html>
 
