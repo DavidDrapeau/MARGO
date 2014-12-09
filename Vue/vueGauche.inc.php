6b18ac02a8d0aff96aee96abda0e281200b60e8f
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2014-12-09 12:30:55 +0100

    Commentaires

--- Vue/vueGauche.inc.php
+++ Vue/vueGauche.inc.php
@@ -34,13 +34,13 @@
                 <li><a href="?controleur=AdminPersonnes&action=creerPersonne"><span class="glyphicon glyphicon-plus-sign"></span> Ajouter une Personne</a></li>
               </ul>
             </li>
-            <li class="dropdown">
+            <!--<li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Promotions <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
                 <li><a href="?controleur=Promotion&action=afficher"><span class="glyphicon glyphicon-align-left"></span> Liste Promotion</a></li>
                 
               </ul>
-            </li>
+            </li>-->
           </ul>
             <ul class="nav navbar-nav navbar-right">
       <?php 
